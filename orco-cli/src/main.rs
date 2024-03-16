93e1f9780f2e31aaea26c302e5729c2748b0ba52
InfiniteCoder <info@infinitecoder.org>
2024-03-16 13:22:19 +0300

    Orco lang

--- orco-cli/src/main.rs
+++ orco-cli/src/main.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 fn main() {
     let mut codebase = orco::Codebase::default();
-    let krate = Path::new("crates/orco-lang/samples/simple.orco");
+    let krate = Path::new("orco-lang/samples/simple.orco");
     orco_lang::Crate::parse(krate, &codebase);
     // let krate = Path::new("crates/orco-rust/samples/simple.rs");
     // let _krate = codebase.add(Box::new(orco_rust::Crate::parse(krate, &codebase)));
