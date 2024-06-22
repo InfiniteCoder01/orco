b236e5e5c5af81980db4ea8047bbe800ba3bc8a4
InfiniteCoder <info@infinitecoder.org>
2024-06-22 22:46:21 +0300

    Semicolon after function body if it's not a block

--- orco-lang/src/parser/symbol/function/mod.rs
+++ orco-lang/src/parser/symbol/function/mod.rs
@@ -6,5 +6,11 @@ pub mod signature;
 /// Parse a function (assumes, that "fn" token is already consumed)
 /// If parse_name is true, function name is expected
 pub fn parse<R: ErrorReporter + ?Sized>(parser: &mut Parser<R>) -> ir::symbol::Function {
-    ir::symbol::Function::new(signature::parse(parser), expression::expect(parser))
+    ir::symbol::Function::new(signature::parse(parser), {
+        let body = expression::expect(parser);
+        if !body.is_block() {
+            parser.expect_operator(Operator::Semicolon);
+        }
+        body
+    })
 }
